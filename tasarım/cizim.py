f03b4056d1ea31ba9c57a174ee5b6e417aa8f1a5
birnuruzun95 <birnuruzun.294976@gmail.com>
2017-05-17 01:28:16 +0300

    canged

--- tasarım/cizim.py
+++ tasarım/cizim.py
@@ -29,10 +29,9 @@ def opamp():
     tosbaga.forward(20)
     tosbaga.left(90)
     tosbaga.forward(20)
-    tosbaga.right(120)
-    tosbaga.forward(60)
-    tosbaga.right(120)
-    tosbaga.forward(60)
+    for i in range(2):
+        tosbaga.right(120)
+        tosbaga.forward(60)
     tosbaga.right(120)
     tosbaga.forward(40)
     tosbaga.backward(20)
@@ -46,7 +45,7 @@ def gnd():
     tosbaga.forward(10)
     tosbaga.backward(20)
     tosbaga.forward(10)
-    #print(tosbaga.position())
+
     tosbaga.penup()
     tosbaga.left(90)
     tosbaga.forward(5)
@@ -65,7 +64,7 @@ def gnd():
     tosbaga.pendown()
     tosbaga.forward(5)
 
-def geribes():
+def geribes1():
     tosbaga.penup()
     tosbaga.setposition(-256, 120)
     tosbaga.right(180)
@@ -97,6 +96,23 @@ def geribes():
     tosbaga.forward(20)
     tosbaga.backward(60)
     tosbaga.right(180)
+    print(tosbaga.position())
+def geribes2():
+    tosbaga.penup()
+    tosbaga.setposition(-214.69, 119.63)
+    tosbaga.pendown()
+    tosbaga.right(90)
+    tosbaga.forward(50)
+    tosbaga.right(90)
+    tosbaga.forward(50)
+    direnc()
+    tosbaga.forward(50)
+    tosbaga.right(90)
+    tosbaga.forward(60)
+    tosbaga.right(90)
+    tosbaga.forward(30)
+    tosbaga.backward(80)
+    tosbaga.right(180)
 
 def integral_alan(tosbaga):
     tosbaga.penup()
@@ -105,7 +121,59 @@ def integral_alan(tosbaga):
     direnc()
     opamp()
     gnd()
-    geribes()
-integral_alan(tosbaga)
+    geribes1()
+
+    tosbaga.penup()
+    tosbaga.setposition(-190.69, 110.63)
+    tosbaga.pendown
+    tosbaga.write("+", font=("arial",14,"bold"))
+    tosbaga.penup()
+    tosbaga.setposition(-187.69, 90.63)
+    tosbaga.pendown
+    tosbaga.write("-", font=("arial", 14, "bold"))
+    tosbaga.penup()
+    tosbaga.setposition(-84.69,109.63)
+    tosbaga.pendown
+
+#integral_alan(tosbaga)
+
+size = 2
+def toplayici(tosbaga):
+    tosbaga.penup()
+    tosbaga.setposition(-256, 120)
+    tosbaga.pendown()
+    direnc()
+
+    knm = 70
+    for i in range(size):
+        tosbaga.penup()
+        tosbaga.setx(-256)
+        tosbaga.sety(knm)
+        tosbaga.pendown()
+        direnc()
+        knm=knm-50
+    tosbaga.left(90)
+    for i in range(size):
+        tosbaga.forward(50)
+    tosbaga.right(90)
+    tosbaga.forward(40)
+    opamp()
+    gnd()
+    geribes2()
+
+    tosbaga.penup()
+    tosbaga.setposition(-150.69, 110.63)
+    tosbaga.pendown
+    tosbaga.write("+", font=("arial", 14, "bold"))
+    tosbaga.penup()
+    tosbaga.setposition(-147.69, 90.63)
+    tosbaga.pendown
+    tosbaga.write("-", font=("arial", 14, "bold"))
+    tosbaga.penup()
+    tosbaga.setposition(-23.38,109.26)
+    tosbaga.pendown
+
+toplayici(tosbaga)
+
 
 wn.mainloop()
